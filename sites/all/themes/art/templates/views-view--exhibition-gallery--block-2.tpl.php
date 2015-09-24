2d03a7d435671fc3f9a3a72497ab98e53c78b76e
bruria1 <bruria@tikke.co.il>
2015-09-24 10:46:25 +0300

    responsive gallery

--- sites/all/themes/art/templates/views-view--exhibition-gallery--block-2.tpl.php
+++ sites/all/themes/art/templates/views-view--exhibition-gallery--block-2.tpl.php
@@ -70,7 +70,6 @@
     owlBlock.each(function(i,n){
       var isRtl = jQuery(n).parents("body").hasClass("i18n-he");
       jQuery(n).owlCarousel({
-        rtl:isRtl,
         loop:true,
         margin:10,
         nav:true,
